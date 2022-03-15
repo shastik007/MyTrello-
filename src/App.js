19aa2ef3cbc7e7e489a9e64795ca0f51e4c89dca
jetigen2003 <abdillaevjetigen127@gmail.com>
2022-03-15 21:16:00 +0600

    done for to day

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Login from './components/layout/Login/Login'
 import HomePage from './components/layout/HomePage/HomePage'
 
 function App() {
-	return <div className='App'>{<HomePage />}</div>
+	return (
+		<div className='App'>
+			<HomePage />
+		</div>
+	)
 }
 
 export default App
