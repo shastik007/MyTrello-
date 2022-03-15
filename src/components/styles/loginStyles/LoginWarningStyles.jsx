ecbfd6ed8e416c4de678541aa6f1bfb342b8ca74
jetigen2003 <abdillaevjetigen127@gmail.com>
2022-03-15 16:46:57 +0600

    save

--- src/components/styles/loginStyles/LoginWarningStyles.jsx
+++ src/components/styles/loginStyles/LoginWarningStyles.jsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
-const LoginWarningStyles = styled.div``
+const LoginWarningStyles = styled.div`
+	& h4 {
+		color: red;
+	}
+`
 
 export default LoginWarningStyles
