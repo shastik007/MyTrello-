a68f6e4ed36360b251136edb0bc367b73d80dca5
jetigen2003 <abdillaevjetigen127@gmail.com>
2022-03-16 19:58:42 +0600

    save

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'
 
 console.log(process.env)
+
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
